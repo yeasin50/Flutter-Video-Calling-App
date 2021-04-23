6fcc0714d4e8619b3475c20a927946f42d689819
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-04-23 15:19:14 +0600

    scaleTransaction with anim handler +hide

--- lib/screens/chat_screen/components/anim_tester.dart
+++ lib/screens/chat_screen/components/anim_tester.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class AnimTester extends StatefulWidget {
   @override
@@ -21,53 +24,96 @@ class _AnimTesterState extends State<AnimTester>
   );
 
   _open() {
-    if (_isOpen)
-      _controller.reverse();
-    else
+    if (!_isOpen) {
+      setState(() => _isOpen = true);
       _controller.forward();
-    setState(() {
-      _isOpen = !_isOpen;
-    });
+    } else {
+      _controller.reverse();
+      Timer(Duration(milliseconds: 400), () {
+        _animation.addListener(() {
+          SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
+            print(
+                "Anim: ${_animation.status} Controller: ${_controller.status}");
+            if (_animation.isDismissed)
+              setState(() {
+                _isOpen = false;
+              });
+          });
+        });
+      });
+    }
+
     print("isOpne:=> $_isOpen");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: Center(
-        child: Container(
-          width: 300,
-          height: 300,
-          color: Colors.yellow.withOpacity(.3),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButton(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.yellow.withOpacity(.3),
+              child: IconButton(
                 icon: Icon(
                   _isOpen ? Icons.close : Icons.add_photo_alternate_outlined,
                 ),
                 onPressed: _open,
               ),
-              AnimatedContainer(
-                width: _isOpen ? _width : 0,
-                curve: Curves.bounceInOut,
-                alignment: Alignment.centerLeft,
-                duration: Duration(milliseconds: 400),
-                child: FittedBox(
-                  child: ScaleTransition(
-                    scale: _controller,
-                    child: actionButton(),
+            ),
+            if (_isOpen)
+              ScaleTransition(
+                scale: _controller,
+                alignment: Alignment.center,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
                   ),
                 ),
               ),
-              Expanded(
-                  child: Container(
-                color: Colors.green,
-              ))
-            ],
-          ),
+            Container(
+              height: 100,
+              color: Colors.green,
+            )
+          ],
         ),
       ),
+    );
+  }
+
+  Row buildRowExpandButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: Icon(
+            _isOpen ? Icons.close : Icons.add_photo_alternate_outlined,
+          ),
+          onPressed: _open,
+        ),
+        AnimatedContainer(
+          width: _isOpen ? _width : 0,
+          curve: Curves.bounceInOut,
+          alignment: Alignment.centerLeft,
+          duration: Duration(milliseconds: 400),
+          child: FittedBox(
+            child: ScaleTransition(
+              scale: _controller,
+              child: actionButton(),
+            ),
+          ),
+        ),
+        Expanded(
+            child: Container(
+          color: Colors.green,
+        ))
+      ],
     );
   }
 }
