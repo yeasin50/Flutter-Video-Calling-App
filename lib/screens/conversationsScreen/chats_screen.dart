872d4816f564b3188a3397ea9b1a6905e090b8ee
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-04-29 21:20:12 +0600

    renamedMainScreen

--- lib/screens/conversationsScreen/chats_screen.dart
+++ lib/screens/conversationsScreen/chats_screen.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'body.dart';
 
-class OpenningScreen extends StatelessWidget {
-  static const String routeName = "/Conversations";
-  OpenningScreen({Key? key}) : super(key: key);
+class ConversationListScreen extends StatelessWidget {
+  static const String routeName = "/ConversationListScreen";
+  ConversationListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
